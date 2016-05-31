ddc716946562dbbe694a64db1d40b8fdde516c00
martin <tlkuo.frb@gmail.com>
2016-05-31 15:49:03 +0800

    Initial react & webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['./src/main.js']
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
