b4bb216872d6b1319485158af1cfce8f6c406a4d
martin <tlkuo.frb@gmail.com>
2016-06-02 17:08:55 +0800

    Add plugins in production config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,19 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       {
